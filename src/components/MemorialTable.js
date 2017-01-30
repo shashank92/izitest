cb541a35070ec0118f0eb06d6fa9dfe06bd9f14a
Shashank Gupta <shashankgupta3@gmail.com>
2017-01-30 11:05:22 -0500

    overflow x scroll for memorial table

--- src/components/MemorialTable.js
+++ src/components/MemorialTable.js
@@ -5,26 +5,28 @@ import { Table } from 'react-bootstrap';
 export default class MemorialTable extends React.Component {
   render() {
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Creation Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.memorials.map((memorial, index) => (
-            <tr key={index}>
-              <td>
-                { formatName(memorial.name, this.props.sortOrder) }
-              </td>
-              <td>
-                { new Date(memorial.creationDate).toLocaleString() }
-              </td>
+      <div className="MemorialTable">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Creation Date</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {this.props.memorials.map((memorial, index) => (
+              <tr key={index}>
+                <td>
+                  { formatName(memorial.name, this.props.sortOrder) }
+                </td>
+                <td>
+                  { new Date(memorial.creationDate).toLocaleString() }
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
     )
   }
 }
