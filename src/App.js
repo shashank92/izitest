8cd9dcfa41e8723145cb78969558c80b025eff91
Shashank Gupta <shashankgupta3@gmail.com>
2017-01-30 10:53:45 -0500

    fixing column widths

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-8 col-xs-offset-2 App-column">
+          <div className="col-sm-8 col-sm-offset-2 col-xs-10 col-xs-offset-1 App-column">
             <h2>Sorting Memorials</h2>
             <h5>React + Redux project for IZI Mobile</h5>
             <AsyncWrapper>
